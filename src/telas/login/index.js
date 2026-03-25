6487c876215bf6c583f986fc33e7f7fa2b804581
Luis Fernando Rodrigues Bueno <77luisnando@gmail.com>
2026-03-25 20:07:49 -0300

    25/03
    Tela de Login feita, apenas faltando os icons

--- src/telas/login/index.js
+++ src/telas/login/index.js
@@ -1,53 +1,48 @@
 import React from "react";
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { Feather } from "@expo/vector-icons";
 import styles from "./styles";
 
 export default function Login() {
-
   const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
-
-      <Text style={styles.titulo}>ENTRAR</Text>
-
-      <View style={styles.card}>
+      <View style={styles.frame}>
 
         <Image
           source={require("../../../assets/logo.png")}
           style={styles.logo}
         />
 
-        {/* Campo usuário */}
-        <View style={styles.inputContainer}>
-          <Feather name="user" size={20} color="#555" />
+        <View style={styles.inputBox}>
+          <View style={styles.inputIcon} />
           <TextInput
             placeholder="Usuário"
-            style={styles.input}
+            style={styles.inputField}
+            placeholderTextColor="#686565"
           />
         </View>
 
-        {/* Campo senha */}
-        <View style={styles.inputContainer}>
-          <Feather name="lock" size={20} color="#555" />
+        <View style={styles.inputBox2}>
+          <View style={styles.inputIcon2} />
           <TextInput
             placeholder="Senha"
             secureTextEntry
-            style={styles.input}
+            style={styles.inputField}
+            placeholderTextColor="#686565"
           />
         </View>
 
-        <TouchableOpacity>
-          <Text style={styles.esqueciSenha}>Esqueci minha senha</Text>
+        <TouchableOpacity style={styles.forgotArea} onPress={() => {}}>
+          <Text style={styles.forgotText}>Esqueci minha senha</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
-          style={styles.botao}
+          style={styles.button}
           onPress={() => navigation.navigate("MyTabs")}
         >
-          <Text style={styles.textoBotao}>ENTRAR</Text>
+          <Text style={styles.buttonText}>ENTRAR</Text>
         </TouchableOpacity>
 
       </View>
--- src/telas/login/styles.js
+++ src/telas/login/styles.js
@@ -1,70 +1,146 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
-    backgroundColor: "#6b6b6b",
+    backgroundColor: "#F4F4F4",
     alignItems: "center",
     justifyContent: "center",
   },
 
-  titulo: {
-    color: "#fff",
-    fontSize: 16,
-    marginBottom: 10,
-  },
-
-  card: {
-    width: "85%",
-    backgroundColor: "#eee",
-    borderRadius: 5,
-    padding: 25,
-    alignItems: "center",
+  frame: {
+    width: 393,
+    height: 852,
+    backgroundColor: "#F4F4F4",
+    borderWidth: 1,
+    borderColor: "#DDDDDD",
+    overflow: "hidden",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 4,
+    position: "relative",
   },
 
   logo: {
-    width: 100,
-    height: 100,
-    marginBottom: 30,
-    resizeMode: "contain",
+    position: "absolute",
+    width: 200,
+    height: 200,
+    top: 95,
+    left: 97,
+    resizeMode: "cover",
   },
 
-  inputContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    borderWidth: 1,
-    borderColor: "#2f3e46",
+  inputBox: {
+    position: "absolute",
+    width: 300,
+    height: 39,
+    left: 46,
+    top: 375,
+    backgroundColor: "#fff",
     borderRadius: 10,
-    width: "100%",
-    paddingHorizontal: 10,
-    marginBottom: 15,
+    borderWidth: 2,
+    borderColor: "#253B49",
+    justifyContent: "center",
   },
 
-  input: {
-    flex: 1,
-    height: 45,
-    marginLeft: 10,
+  inputIcon: {
+    position: "absolute",
+    width: 25,
+    height: 25,
+    left: 5,
+    top: 7,
+    backgroundColor: "#686565",
+    borderRadius: 4,
   },
 
-  esqueciSenha: {
-    fontSize: 12,
-    color: "#555",
-    marginBottom: 20,
+  inputLabel: {
+    position: "absolute",
+    left: 35,
+    top: 8,
+    color: "#686565",
+    fontSize: 16,
+    fontFamily: "Roboto",
+    fontWeight: "300",
+  },
+
+  inputField: {
+    position: "absolute",
+    left: 35,
+    right: 5,
+    top: 7,
+    height: 25,
+    color: "#000",
+    fontSize: 16,
+    padding: 0,
+    margin: 0,
+  },
+
+  inputBox2: {
+    position: "absolute",
+    width: 300,
+    height: 39,
+    left: 46,
+    top: 444,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "#253B49",
+    justifyContent: "center",
+  },
+
+  inputIcon2: {
+    position: "absolute",
+    width: 25,
+    height: 25,
+    left: 5,
+    top: 7,
+    backgroundColor: "#686565",
+    borderRadius: 4,
+  },
+
+  forgotArea: {
+    position: "absolute",
+    left: 131,
+    top: 503,
+    width: "auto",
+    alignItems: "center",
+  },
+
+  forgotText: {
+    color: "#2D2D2D",
+    fontSize: 14,
+    fontFamily: "Roboto",
+    fontWeight: "200",
     textDecorationLine: "underline",
+    textAlign: "center",
+    flexWrap: "nowrap",
+    includeFontPadding: false,
   },
 
-  botao: {
-    backgroundColor: "#2f3e46",
-    width: "100%",
-    padding: 12,
-    borderRadius: 10,
+  button: {
+    position: "absolute",
+    left: 148,
+    top: 589,
+    paddingTop: 10.5,
+    paddingBottom: 10.7,
+    paddingLeft: 16,
+    paddingRight: 16,
+    backgroundColor: "#1D3545",
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: "#253B49",
     alignItems: "center",
+    justifyContent: "center",
   },
 
-  textoBotao: {
+  buttonText: {
     color: "#fff",
-    fontWeight: "bold",
+    fontSize: 16,
+    fontFamily: "Raleway",
+    fontWeight: "500",
+    lineHeight: 19.2,
   },
 });
 
